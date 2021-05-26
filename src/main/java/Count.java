185ead384cf12efc1805bd23fb1b0af1004f774c
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-26 12:13:38 -0500

    feat: finished with servlets exercise

---
+++ src/main/java/Count.java
@@ -0,0 +1,19 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "MarcoCount", urlPatterns = "/count") // metadata to go along with our servlet
+
+public class Count extends HttpServlet {
+    int count = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        count++;
+        response.getWriter().println("<h1>You are the " + count + " visitor to this site!</h1>");
+
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+@WebServlet(name = "MarcoHelloWorld", urlPatterns = "/hello") // metadata to go along with our servlet
+public class HelloWorldServlet extends HttpServlet{ //defining the class name / extending from HttpServlet
+    // Put doGet and doPost methods in here
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("<h1>Hello World!</h1>");
+        response.getWriter().println("<h1>copacetic</h1>");
+
+        String name = request.getParameter("name");
+
+        if(name == null){
+            name = "Hello world!";
+        }
+
+        response.getWriter().println("<h1>Hello " + name + "!" + "</h1>");
 
     }
 }
